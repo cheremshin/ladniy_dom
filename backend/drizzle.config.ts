3ec07c7764ad6db5c6c07f4833160deec25fcb38
Sergei Cheremisin <sergey.cheremshin@gmail.com>
2025-12-04 16:33:20 +0700

    fix: Подключение конфигурации Drizzle к проекту

--- backend/drizzle.config.ts
+++ backend/drizzle.config.ts
@@ -1,4 +1,7 @@
 import { defineConfig } from 'drizzle-kit';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 export default defineConfig({
     schema: './src/database/schema/*',
@@ -6,7 +9,7 @@ export default defineConfig({
     dialect: 'postgresql',
     dbCredentials: {
         host: process.env.DB_HOST!,
-        port: Number(process.env.DB_PORT)!,
+        port: Number(process.env.DB_PORT!),
         user: process.env.DB_USER!,
         password: process.env.DB_PASSWORD!,
         database: process.env.DB_NAME!,
