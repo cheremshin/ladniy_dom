6d680c6da6cbf7156b5afcb89250e243cfa3d363
Sergei Cheremisin <sergey.cheremshin@gmail.com>
2025-12-26 19:09:04 +0700

    fix: Добавление dotenv для чтения параметров, вынос app в корень модулей

--- backend/src/modules/app.module.ts
+++ backend/src/modules/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
-import { AppResolver } from '@/modules/app/presentation/app.resolver';
+import { AppResolver } from '@/modules/app.resolver';
+import { DatabaseModule } from '../database/database.module';
+import { CategoriesModule } from './categories/categories.module';
 
 @Module({
     imports: [
+        DatabaseModule,
+        CategoriesModule,
         GraphQLModule.forRoot<ApolloDriverConfig>({
             driver: ApolloDriver,
             autoSchemaFile: true,
