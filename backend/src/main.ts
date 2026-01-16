929b32859b911d2d760ff6f61adbc1421cd710da
Sergei Cheremisin <sergey.cheremshin@gmail.com>
2026-01-16 18:29:35 +0700

    feat: Добавление ValidationPipe

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app.module';
 import * as dotenv from 'dotenv';
+import { ValidationPipe } from '@nestjs/common';
 
 dotenv.config();
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+
+    app.useGlobalPipes(
+        new ValidationPipe({
+            transform: true,
+            whitelist: true,
+            forbidNonWhitelisted: true,
+            disableErrorMessages: process.env.NODE_ENV === 'production',
+        }),
+    );
+
     await app.listen(process.env.PORT ?? 3000);
     console.log(`Application is running on: ${await app.getUrl()}`);
 }
