87f191d9e67a516bea49db8bda45568eb46c01cc
Sergei Cheremisin <sergey.cheremshin@gmail.com>
2026-01-17 00:14:06 +0700

    fix: Теперь поля опциональные

--- backend/src/common/presentation/dto/pagination.args.ts
+++ backend/src/common/presentation/dto/pagination.args.ts
@@ -1,13 +1,17 @@
 import { ArgsType, Field, Int } from '@nestjs/graphql';
-import { Max, Min } from 'class-validator';
+import { IsInt, IsOptional, Max, Min } from 'class-validator';
 
 @ArgsType()
 export class PaginationArgs {
     @Field(() => Int, { defaultValue: 1 })
+    @IsOptional()
+    @IsInt()
     @Min(1)
     page: number = 1;
 
     @Field(() => Int, { defaultValue: 20 })
+    @IsOptional()
+    @IsInt()
     @Min(1)
     @Max(100)
     limit: number = 20;
