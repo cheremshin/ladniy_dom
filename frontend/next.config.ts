3a4963971eef7a71f8efdd5f6a2c105b2fd54ce6
Sergei Cheremisin <sergey.cheremshin@gmail.com>
2026-01-30 15:26:04 +0700

    chore: Разрешен адрес backend для получения изображений в dev-режиме

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,6 +3,16 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
     reactCompiler: true,
     output: 'standalone',
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'http',
+                hostname: 'localhost',
+                port: '3000',
+                pathname: '/files/**'
+            },
+        ],
+    },
 };
 
 export default nextConfig;
