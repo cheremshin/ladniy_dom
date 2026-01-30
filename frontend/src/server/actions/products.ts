ebe673992d15273f0059909504607748a1112a18
Sergei Cheremisin <sergey.cheremshin@gmail.com>
2026-01-30 15:29:23 +0700

    feat: Реализация серверных actions для получения продуктов и продукта

---
+++ frontend/src/server/actions/products.ts
@@ -0,0 +1,23 @@
+'use server';
+
+import { CatalogProductsQuery, CatalogProductsQueryVariables, ProductPageQuery, ProductPageQueryVariables } from '@/shared/api/graphql/__generated__/types';
+import { graphqlFetch } from '../apollo/client/apollo-server-client';
+import { CATALOG_PRODUCTS, PRODUCT_PAGE } from '@/shared/api/graphql/operations/products';
+
+export async function getCatalogProducts(variables?: CatalogProductsQueryVariables) {
+    const data = await graphqlFetch<CatalogProductsQuery, CatalogProductsQueryVariables>(
+        CATALOG_PRODUCTS,
+        variables ?? {},
+    );
+
+    return data;
+}
+
+export async function getProductBySlug(slug: string) {
+    const data = await graphqlFetch<ProductPageQuery, ProductPageQueryVariables>(
+        PRODUCT_PAGE,
+        { slug },
+    );
+
+    return data?.productBySlug ?? null;
+}
