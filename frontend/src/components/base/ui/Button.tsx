1cf80428c5ed5522c845736b0136132f8f568f12
Sergei Cheremisin <sergey.cheremshin@gmail.com>
2026-02-03 23:20:20 +0700

    feat: Поддержка основного и второстепенного стиля

--- frontend/src/components/base/ui/Button.tsx
+++ frontend/src/components/base/ui/Button.tsx
@@ -4,15 +4,18 @@ import clsx from 'clsx';
 
 import './Button.styles.css';
 
-type PropsT = React.ComponentProps<'button'>;
+type PropsT = React.ComponentProps<'button'> & {
+    variant?: 'primary' | 'secondary';
+};
 
 export const Button: FC<PropsT> = ({
     className,
     children,
+    variant = 'primary',
     ...props
 }) => (
     <button
-        className={clsx('base-button', className)}
+        className={clsx('base-button', `base-button-${variant}`, className)}
         type='button'
         {...props}
     >
