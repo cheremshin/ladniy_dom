b185877cd9609970449d6177d66a7d461402a795
Sergei Cheremisin <sergey.cheremshin@gmail.com>
2026-02-03 23:23:20 +0700

    fix: Исправление списка на серверный ProductCard, превращение в серверный компонент

--- frontend/src/components/widgets/productsCatalog/ui/ProductsListView.tsx
+++ frontend/src/components/widgets/productsCatalog/ui/ProductsListView.tsx
@@ -1,8 +1,5 @@
-'use client';
-
-import { ProductCardView } from '@/components/entities/product/ui/ProductCard/ProductCardView';
+import { ProductCard } from '@/components/entities/product/productCard/server/ProductCard';
 import { ProductPreview } from '@/shared/entities/product.types';
-import { FC } from 'react';
 
 import './ProductsListView.styles.css';
 
@@ -11,18 +8,16 @@ type PropsT = {
     productPageBaseUrl: string;
 };
 
-export const ProductsListView: FC<PropsT> = ({ products, productPageBaseUrl }) => (
-    <div className='products-list-view'>
-        {products.map((product) => (
-            <ProductCardView
-                key={product.id}
-                {...product}
-                productPageUrl={`${productPageBaseUrl}/${product.slug}`}
-                buyButtonLabel='В корзину'
-                onBuyButtonClick={() => console.log(product.slug)}
-                onFavouriteButtonClick={() => console.log(product.slug)}
-                isFavourite={false}
-            />
-        ))}
-    </div>
-);
+export async function ProductsListView({ products, productPageBaseUrl }: PropsT) {
+    return (
+        <div className='products-list-view'>
+            {products.map((product) => (
+                <ProductCard
+                    key={product.id}
+                    product={product}
+                    productPageUrl={`${productPageBaseUrl}/${product.slug}`}
+                />
+            ))}
+        </div>
+    );
+}
