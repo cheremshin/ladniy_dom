88d6d618b39ed7ad865bf94e702d72ca7c4a60a8
Sergei Cheremisin <sergey.cheremshin@gmail.com>
2026-02-06 18:24:01 +0700

    refactor: Лимит страниц опционален

--- backend/src/common/presentation/dto/pagination.args.ts
+++ backend/src/common/presentation/dto/pagination.args.ts
@@ -7,12 +7,12 @@ export class PaginationArgs {
     @IsOptional()
     @IsInt()
     @Min(1)
-    page: number = 1;
+    page?: number = 1;
 
-    @Field(() => Int, { defaultValue: 20 })
+    @Field(() => Int, { nullable: true })
     @IsOptional()
     @IsInt()
     @Min(1)
     @Max(100)
-    limit: number = 20;
+    limit?: number;
 }
