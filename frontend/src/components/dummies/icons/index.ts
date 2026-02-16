d5ddf769e008183204087a31f4f5c01a836e154b
Sergei Cheremisin <sergey.cheremshin@gmail.com>
2026-02-16 22:59:02 +0700

    feat: Добавление иконки редактирования

--- frontend/src/components/dummies/icons/index.ts
+++ frontend/src/components/dummies/icons/index.ts
@@ -4,5 +4,6 @@ import { SearchIcon } from './search/SearchIcon';
 import { CartIcon } from './cart/CartIcon';
 import { ProfileIcon } from './profile/ProfileIcon';
 import { DeleteIcon } from './delete/DeleteIcon';
+import { EditIcon } from './edit/EditIcon';
 
-export { CategoriesIcon, FavouriteIcon, SearchIcon, CartIcon, ProfileIcon, DeleteIcon };
+export { CategoriesIcon, FavouriteIcon, SearchIcon, CartIcon, ProfileIcon, DeleteIcon, EditIcon };
